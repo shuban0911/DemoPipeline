6a80ae18769d0d6ba5a25b0016830028198b86e8
shuban0911 <uma.sankari09@gmail.com>
2023-03-21 17:41:11 -0400

    Sample pipeline

--- src/test/java/programs/HelloWorld.java
+++ src/test/java/programs/HelloWorld.java
@@ -1,9 +1,31 @@
 package programs;
 
-public class HelloWorld {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloWorld {
+	
+	@Test
+	public void sampleTest() {
+		WebDriver driver = null;
+		
+		String browser = System.getProperty("browser","firefox");
+		if(browser.contains("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			 driver = new ChromeDriver();
+			 driver.get("https://www.google.com");
+		}else {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.get("https://www.google.com");
+		}
+		
+		
+		System.out.println(browser+"*********************");
 	}
 
 }
